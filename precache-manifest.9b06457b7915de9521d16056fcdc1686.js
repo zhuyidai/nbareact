72d44d07cfe8d75968216328062ac0741ca0ff8e
zhuyidai <zhuyidai>
2020-01-31 16:06:38 -0500

    Updates

---
+++ precache-manifest.9b06457b7915de9521d16056fcdc1686.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06144710cbff9b306de4a302ec247f4e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d9dae28edb50fbfcd886",
+    "url": "./static/js/2.5d7180ca.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "./static/js/2.5d7180ca.chunk.js.LICENSE"
+  },
+  {
+    "revision": "95f2174bcded5eebe68d",
+    "url": "./static/js/3.a8a487fa.chunk.js"
+  },
+  {
+    "revision": "725fdb9742952cea97c0",
+    "url": "./static/js/main.5a41005a.chunk.js"
+  },
+  {
+    "revision": "03d886ebd364fa8d89b7",
+    "url": "./static/js/runtime-main.b983b469.js"
+  },
+  {
+    "revision": "a71d303bb77b372bc335c0ef8264566b",
+    "url": "./static/media/kobe1.a71d303b.jpg"
+  },
+  {
+    "revision": "4c68c8141e07004261dc3c306c8d3723",
+    "url": "./static/media/nba1.4c68c814.png"
+  }
+]);
